593ece862296fa78135abb62d3283cc710ab29d5
Miguel Ponce <mponce1@uoc.edu>
2024-04-04 06:44:34 -0500

    aad cahnges

--- packages/evershop/src/modules/catalog/pages/admin/productNew/index.js
+++ packages/evershop/src/modules/catalog/pages/admin/productNew/index.js
@@ -1,9 +1,9 @@
 const {
-  setContextValue
-} = require('../../../../graphql/services/contextHelper');
-const {
   translate
 } = require('@evershop/evershop/src/lib/locale/translate/translate');
+const {
+  setContextValue
+} = require('../../../../graphql/services/contextHelper');
 // eslint-disable-next-line no-unused-vars
 module.exports = (request, response) => {
   setContextValue(request, 'pageInfo', {
